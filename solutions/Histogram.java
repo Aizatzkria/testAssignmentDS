8b64d9c9479d9631a7ffd68e27f9df21b6dff6e6
Aizatzkria <93086166+Aizatzkria@users.noreply.github.com>
2022-06-18 23:12:33 +0800

    Add files via upload

---
+++ solutions/Histogram.java
@@ -0,0 +1,62 @@
+package histogram;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.*;
+
+public class Histogram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int amount = sc.nextInt();
+        for (int j = 0; j < amount; j++) {
+
+            int testCases = sc.nextInt();
+            int bins = sc.nextInt();
+            int max = 0;
+            int min = Integer.MAX_VALUE;
+
+            Stack<Integer> dataPoints = new Stack<>();
+            int[] counts = new int[bins];
+            int[] interval = new int[bins + 1];
+            
+            for (int i = 0; i < testCases; i++) {
+                int l = sc.nextInt();
+                max = Math.max(l, max);
+                min = Math.min(l, min);
+                dataPoints.push(l);
+            }
+            
+            int range = (max - min) / bins;
+            
+            for (int i = 0; i < bins + 1; i++) {
+                interval[i] = (range * i + min);
+            }
+            
+            interval[bins - 1]++;
+            while (!dataPoints.isEmpty()) {
+                for (int i = 0; i < bins; i++) {
+                    if (dataPoints.peek() < interval[i] + range) {
+                        counts[i]++;
+                        dataPoints.pop();
+                        break;
+                    }
+                }
+            }
+            interval[bins - 1]--;
+            
+            String cutoffs = "";
+            String count = "";
+            
+            for (int i = 0; i < interval.length; i++) {
+                cutoffs = cutoffs + interval[i] + " ";
+            }
+            for (int i = 0; i < counts.length; i++) {
+                count = count + counts[i] + " ";
+            }
+            
+            System.out.println(cutoffs);
+            System.out.println(count);
+        }
+    }
+}
